da873ffc7feafcb780c79b6e3b87f85b998bc6a7
Rico4eT <shiwawa96@mail.ru>
2024-01-30 18:53:37 +0500

    My Final Task For Module 4 With Loops

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for (int i = 1; i <= 10; i++) {
+            String digit = String.valueOf(numberTableToPrint);
+            String iterator = String.valueOf(i);
+            System.out.println(iterator + " x " + digit + " = " + i * numberTableToPrint);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = i; j < cathetusLength; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print(i - j + 1);
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
 
     }
 
